e864b5874e11b7bf5d064082378bc5bcdf41b121
divanshu khandelwal <divanshukhandelwal79@gmail.com>
2018-08-05 10:27:11 +0530

    Add files via upload

---
+++ pkl files/User_data/123.py
@@ -0,0 +1,10 @@
+import pandas as pd
+##df = pd.DataFrame(columns=list('A'))
+df = pd.read_pickle('Divanshu.pkl')
+##df.append({'A':'Divanshu'},ignore_index=True)
+##df.append({'A':'abvc'},ignore_index=True)
+##df.append({'A':'xyz'},ignore_index=True)
+##
+##df.to_pickle('Ashna.pkl')
+print(df)
+##df = pd.read_pickle('All_user.pkl')
